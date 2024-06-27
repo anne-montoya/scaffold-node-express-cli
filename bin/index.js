96c5676cc46aedd637b83833d7d736272ef431b1
Anne Montoya <annekristinemontoya@gmail.com>
2024-06-27 18:32:58 +0800

    copy template files

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
-
+import fs from 'node:fs'
+import { promisify } from 'node:util'
+import path from 'node:path'
 import chalk from 'chalk'
 import figlet from 'figlet'
 import { program } from 'commander'
 import inquirer from 'inquirer'
+import ncp from 'ncp'
+import { execa } from 'execa'
+
+const access = promisify(fs.access)
+const copy = promisify(ncp)
 
 console.log(
     chalk.green(figlet.textSync('Node-Express Scaffold', { horizontalLayout: 'full'} ))
@@ -38,9 +45,64 @@ parameters.port = options.port ?? visiblePrompts.push(prompts.port)
 if (visiblePrompts.length) {
     inquirer
         .prompt(visiblePrompts)
-        .then((answers) => {
+        .then(async (answers) => {
             parameters = { ...parameters, ...answers }
             console.log(parameters)
+            await copyTemplate(parameters)
+            // await cleanTemplateFiles(parameters)
+            console.log('%s Project ready', chalk.green.bold('DONE'))
             // replace all occurance of name and port inside a folder
         })
+}
+
+async function copyTemplate(options) {
+    const dir = {
+        templateDirectory: '',
+        targetDirectory: path.resolve(process.cwd(), options.name)
+    }
+
+    const currentFileUrl = import.meta.url
+    dir.templateDirectory = path.resolve(
+        new URL(currentFileUrl).pathname,
+        '../../template'
+    )
+
+    console.log(dir.templateDirectory)
+
+    try {
+        await access(dir.templateDirectory, fs.constants.R_OK)
+    } catch(err) {
+        console.log(err)
+        console.error('%s Invalid template name', chalk.red.bold('ERROR'))
+        process.exit(1)
+    }
+
+    await execa('mkdir', [options.name], { cwd: process.cwd() })
+    await copyTemplateFiles(dir)
+}
+
+
+async function copyTemplateFiles(options) {
+    return copy(options.templateDirectory, options.targetDirectory, {
+        clobber: false
+    })
+}
+
+async function cleanTemplateFiles(options) {
+    console.log('CWD: ',process.cwd())
+    // const result = await execa(`find ${options.name} -type f -exec sed -i '' -e "s/"#{name}"/"${options.name}"/g" {} +`)
+    const result = await execa(`find * -type f -exec sed -i '' -e "s/"#{name}"/"${options.name}"/g" {} +`, {cwd: path.resolve(process.cwd(), options.name)} )
+    if (result.failed) {
+        return Promise.reject(new Error('Failed to set project name'))
+    }
+}
+async function initGit(options) {
+    const result = await execa('git', ['init'], {
+        cwd: options.targetDirectory
+    })
+    if (result.failed) {
+        return Promise.reject(new Error('Failed to initialize git'))
+    }
+
+    return
 }
