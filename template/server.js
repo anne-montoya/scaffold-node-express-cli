ec542ba5374d500edb73a168ae23b7650d27f9a0
Anne Montoya <annekristinemontoya@gmail.com>
2024-07-15 22:27:31 +0800

    1.0.3

--- template/server.js
+++ template/server.js
@@ -2,6 +2,11 @@ import express from 'express'
 
 const app = express(),
     port = "#{port}"
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Server running!' })
+})
+
 app.get('/health', (req, res) => {
     res.json({ message: 'Healthy!' })
 })
